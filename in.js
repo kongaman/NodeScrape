85a390d060191d90d1aad05279376840ac7ea34e
Chris <gateway.kranz@gmail.com>
2018-05-16 17:15:59 +0200

    Standalone get CoinsList from coinmarketcap-api and build urls

---
+++ in.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const request = require('request');
+const app = express();
+const port = 8080;
+
+const apiURL = 'https://api.coinmarketcap.com/v2/listings/';
+
+request(apiURL, function(err, resp, body){
+    console.log('Getting cmc-API');
+    const allCoins = JSON.parse(body).data;
+    for (let i in allCoins) {
+        let marketsUrl = `https://coinmarketcap.com/currencies/${allCoins[i].website_slug}/#markets`;
+        console.log(marketsUrl);
+    
+    
+    }    
+});
+
+//Setting up server
+app.listen(port, function() {
+    console.log(`app listening on port ${port}`);
+});
